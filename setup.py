729f3aa16894b06581e36b01b7a03ba9f8819ed9
Philippe O. Wagner <wagner@arteria.ch>
2014-10-21 00:56:20 +0200

    Initial.

---
+++ setup.py
@@ -0,0 +1,16 @@
+# -*- encoding: utf-8 -*-
+
+from setuptools import setup
+from setuptools import find_packages
+import threebot as app
+
+setup(
+    name="django-compat",
+    version="0.0.0",
+    author_email="admin@arteria.ch",
+    packages=find_packages(),
+    include_package_data=True,
+
+     
+
+)
