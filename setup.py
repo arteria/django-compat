02d94f25a5bdd30bfe96e0b11d64f54177d2a723
Yannik Ammann <ammann@arteria.ch>
2015-02-17 12:35:18 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,26 @@
 from setuptools import setup
 from setuptools import find_packages
 
+install_requires = [
+    'django>=1.4,<1.9',
+    'six',
+]
+
 setup(
     name="django-compat",
-    version="0.0.1",
+    version="1.0.0",
     author_email="admin@arteria.ch",
     packages=find_packages(),
     include_package_data=True,
-    description="For- and backwards compatibility layer for Django 1.4 to 1.7",
+    description="For- and backwards compatibility layer for Django 1.4 to 1.8",
+    license='MIT',
+    install_requires=install_requires,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Framework :: Django',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+    ],
 )
