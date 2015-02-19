04f3fef75eafc3ba95137708c294b7ddb2a0c9da
Philippe O. Wagner <wagner@arteria.ch>
2015-02-19 14:11:50 +0100

    Version bump 1.0.1

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ install_requires = [
 
 setup(
     name="django-compat",
-    version="1.0.0",
+    version="1.0.1",
     author_email="admin@arteria.ch",
     packages=find_packages(),
     include_package_data=True,
