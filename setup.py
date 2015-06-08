5d93abef8dd9859f356ba850adfc9e456a51a2ad
Philippe O. Wagner <wagner@arteria.ch>
2015-06-08 18:11:14 +0200

    Added md to rst conversion for PyPI
    and updated description

--- setup.py
+++ setup.py
@@ -1,8 +1,35 @@
 # -*- encoding: utf-8 -*-
-
+import os
 from setuptools import setup
 from setuptools import find_packages
 
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+
+
+def get_path(fname):
+    return os.path.join(os.path.dirname(os.path.abspath(__file__)), fname)
+
+
+def read(fname):
+    return open(get_path(fname)).read()
+
+
+try:
+    import pypandoc
+    long_description = pypandoc.convert(get_path('README.md'), 'rst')
+    long_description = long_description.split('<!---Illegal PyPi RST data -->')[0]
+    f = open(get_path('README.rst'), 'w')
+    f.write(long_description)
+    f.close()
+    print("Successfully converted README.md to README.rst")
+except (IOError, ImportError):
+    # No long description... but nevermind, it's only for PyPi uploads.
+    long_description = ""
+    
+    
 install_requires = [
     'django>=1.4,<1.9',
     'six',
@@ -14,7 +41,7 @@ setup(
     author_email="admin@arteria.ch",
     packages=find_packages(),
     include_package_data=True,
-    description="For- and backwards compatibility layer for Django 1.4 to 1.8",
+    description="For- and backwards compatibility layer for Django 1.4, 1.7 and 1.8",
     license='MIT',
     install_requires=install_requires,
     classifiers=[
