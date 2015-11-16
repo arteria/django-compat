9a3cc469ac5aadaec09554ec56a1bf879c72b4c2
Jannis Vamvas <vamvas@arteria.ch>
2015-11-16 11:56:13 +0100

    Bump version 1.0.8

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ install_requires = [
 
 setup(
     name="django-compat",
-    version="1.0.7",
+    version="1.0.8",
     author_email="admin@arteria.ch",
     packages=find_packages(),
     include_package_data=True,
