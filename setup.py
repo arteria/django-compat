597bd81a635dec7fdfcccf09b5c95df63615dbf2
Walter Renner <wltr@posteo.is>
2017-12-04 10:12:37 +0100

    Bump Version

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ except (OSError, IOError):
 
 setup(
     name="django-compat",
-    version="1.0.14",
+    version="1.0.15",
     author='arteria GmbH',
     author_email="admin@arteria.ch",
     packages=find_packages(),
